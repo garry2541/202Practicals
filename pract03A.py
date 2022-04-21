85b4da19c7ea59495e1913b12b305489d7c87fba
Txusif <txusif@gmail.com>
2022-04-21 18:01:17 +0530

    practical 1-8

--- pract03A.py
+++ pract03A.py
@@ -19,21 +19,21 @@ def emailcheck(str):
                 if (re.match(emailpattern,str)):
                     print("Email format is correct :)")
                 else:
-                    print("Email is incorrect!!")
+                    print("Email is incorrect :(")
 
 def pincodecheck(str):
                 pincodepattern="\d\d\d\d\d\d"
                 if(re.match(pincodepattern,str) and len(str)==6):
                     print("pincode format is correct :)")
                 else:
-                    print("pincode is incorrect!!")
+                    print("pincode is incorrect :(")
 
 def phonenocheck(str):
                 phonepattern="\+\d\d\-\d\d\d\d\d\d\d\d\d\d"
                 if(re.match(phonepattern,str) and len(str)==14):
                    print("Phone number pattern is correct :)")
                 else:
-                    print("phone number is incorrect!!")
+                    print("phone number is incorrect :(")
 
 print("--Program to use regular expression--")
 find()
--- pract03B.py
+++ pract03B.py
@@ -1,2 +1,47 @@
 # Practical 03
 # Regular Expression
+
+from re import *
+str = "Global Warming"
+print(str)
+
+print("Last 4 characters: ")
+last4 = findall("\D\D\D\D$", str)
+for ch in last4:
+    print(ch)
+
+print("Characters starting from index 4 and ending at index 8: ")
+mid = findall("^....(.....)", str)
+for ch in mid:
+    print(ch)
+
+print("String contains Alphanumeric character or not: ")
+if match("^(0-9A-Za-z)*$", str):
+    print("True")
+else:
+    print("False")
+
+print("Trim Last 4 characters: ")
+str1 = sub("\D\D\D\D$", " ", str)
+print(str1)
+
+print("Trim First 4 characters: ")
+str2 = sub("^\D\D\D\D", " ", str)
+print(str2)
+
+print("Index of Wa in",str ,":")
+i = str.index("Wa")
+print(i)
+
+print("change case to Title: ")
+str3 = str.title()
+
+# print("Check whether string is in Title case: ")
+# if(match("^[A-Z][a-z]*\s[a-Z][a-z]*", str)):
+#     print(str, "is in Title case")
+# else:
+#     print(str, "is not in Title case")
+
+print("Replace all letter 'a' in the string with '*'")
+str4 = sub("a","*",str)
+print(str4)
